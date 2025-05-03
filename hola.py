6e5e1e0ac2fc1e28da1087b53811f69a2b18c8b6
olidernas4 <nas451@hotmail.es>
2025-05-03 09:35:30 -0500

    esto lo hizo el profe

---
+++ hola.py
@@ -0,0 +1,21 @@
+# letra = "hola"
+
+# for char in letra:
+#     print(char)
+
+# letra = "hola"
+# index = 0
+
+# while index < len(letra):
+#     print(letra[index])
+#     index += 1
+
+# letra = "hola"
+
+# for index, char in enumerate(letra):
+#     print(char)
+
+letra = "hola"
+
+for i in range(len(letra)):
+    print(letra[i])
